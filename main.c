5f9f624d3028e347b35340ded057367b36539c5a
George Kochera <george.kochera@gmail.com>
2021-01-25 21:52:39 -0500

    created makefile

---
+++ main.c
@@ -0,0 +1,12 @@
+/*
+Author: George Kochera
+Date Creatd: 1/25/2021
+Filename: main.c
+Description: Entry point for smallsh
+*/
+
+int main()
+{
+    printf("Welcome to smallsh!\n");
+    return 0;
+}
