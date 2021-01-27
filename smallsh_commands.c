c649880e7195b5abfe1c54249b56e786a29b5b86
George Kochera <george.kochera@gmail.com>
2021-01-26 20:17:01 -0500

    programs now run correctly that are not predefined ones in smallsh

--- smallsh_commands.c
+++ smallsh_commands.c
@@ -8,6 +8,8 @@ Description: Contains the functions for handling the core commands of 'exit', 'c
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 void cmd_exit()
 {
@@ -29,10 +31,18 @@ void cmd_other(char ** tokens)
     pid_t newPid = -5;
     int childStatus = 0;
     newPid = fork();
-    if (newPid == 0)
+    switch (newPid)
     {
-        execvp(tokens[0], tokens + 1);
-        waitpid(newPid, &childStatus, 0);
+        case -1:
+            printf("Invalid command!\n");
+            break;
+        case 0:
+            execvp(tokens[0], tokens);
+            exit(2);
+            break;
+        default:
+            newPid = waitpid(newPid, &childStatus, 0);
+            break;
     }
 
 }
